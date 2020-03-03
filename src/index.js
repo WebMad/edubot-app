6b1029be87246a7aaafbcad24e4f019c3704b2c8
webmad <alecmei.gubin@yandex.ru>
2020-03-04 00:14:35 +0400

    Добавил поддержку ВК

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import connect from '@vkontakte/vkui-connect';
 import * as serviceWorker from './serviceWorker';
+
+connect.send('VKWebAppInit', {});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
